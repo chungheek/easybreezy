25a2d113a50f8bf10f15c542f779885e6aca8df8
Chunghee Kim <chungheek@gmail.com>
2020-07-09 14:58:39 -0400

    Added scss for styling

---
+++ src/API/temperatureApi.js
@@ -0,0 +1,16 @@
+/*
+  Will remove mock data after API has been implemented.
+*/
+const temp = 
+{
+  "main": {
+    "temp": 295.69,
+    "feels_like": 298.13,
+    "temp_min": 294.26,
+    "temp_max": 297.15,
+    "pressure": 1015,
+    "humidity": 88
+  }
+}
+
+module.exports = temp;
---
+++ src/components/Temperature.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import temp from '../API/temperatureApi';
+import './styles.scss';
+
+class Temperature extends Component {
+  render() {
+    return(
+      <div className='temp'>
+        Temperature: {temp.main.temp} <br />
+      </div>
+    );
+  }
+}
+
+export default Temperature;
