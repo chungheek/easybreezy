420941d7210eebf9f8bc67d860e193876195e0d5
Chunghee Kim <chungheek@gmail.com>
2020-07-09 15:56:43 -0400

    Moved Temperature to its own folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Temperature from './components/Temperature';
+import Temperature from './components/Temperature/Temperature';
 
 function App() {
   return (
--- src/components/Temperature/Temperature.jsx
+++ src/components/Temperature/Temperature.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import temp from '../API/temperatureApi';
+import temp from '../../API/temperatureApi';
 import './styles.scss';
 
 class Temperature extends Component {
   render() {
     return(
-      <div className='temp'>
+      <div className="temp">
         Temperature: {temp.main.temp} <br />
       </div>
     );
