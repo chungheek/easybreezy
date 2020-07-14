10aa0319e2d1a5740b550968b855f64083577090
Chunghee Kim <chungheek@gmail.com>
2020-07-14 18:04:12 -0400

    Adding build folder

---
+++ build/precache-manifest.ee54f85ef1ba84c1237c13b4c2f56940.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56305fbf3e2d537fbb4b31ba595521f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6803c8ad620d01e1cb2d",
+    "url": "/static/css/main.491a0736.chunk.css"
+  },
+  {
+    "revision": "d8dd479b0a7a9629f424",
+    "url": "/static/js/2.fa128945.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fa128945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6803c8ad620d01e1cb2d",
+    "url": "/static/js/main.79dfd003.chunk.js"
+  },
+  {
+    "revision": "4e5691b5e6a04faa3911",
+    "url": "/static/js/runtime-main.26a238ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
