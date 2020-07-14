8e8ab8d79c95cccc04f720b8e87f0769ebe3c3c9
Chunghee Kim <chungheek@gmail.com>
2020-07-13 22:03:42 -0400

    Created form input component and API

--- src/components/Temperature/Temperature.jsx
+++ src/components/Temperature/Temperature.jsx
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
-import temp from '../../API/temperatureApi';
-import './styles.scss';
+import './Temperature.scss';
+import FormInput from '../FormInput/FormInput';
 
 class Temperature extends Component {
+  constructor() {
+    super();
+    this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
+    this.state = {
+      api: [],
+      temperature: 303,
+      location: 'Atlanta'
+    }
+  }
+
+  forceUpdateHandler(){
+    this.forceUpdate();
+  };
+
+  componentDidMount() {
+    this.getForecast();
+  }
+
+  formCallBack = (locationData) => {
+    this.getForecast(locationData);
+  }
+
+  getForecast(location='Atlanta') {
+    //const url = 'http://api.openweathermap.org/data/2.5/weather?q=Austin&APPID=dbc4c991f66320a5787180247fb2a641';
+    let url2 = process.env.REACT_APP_OPEN_MAP_API_TODAY + location + process.env.REACT_APP_ID;
+    fetch(url2)
+    .then(response => response.json())
+    .then(response => {
+      this.setState({
+        location: location,
+        temperature: response.main.temp
+      })
+    })
+  }
+
   render() {
     return(
-      <div className="temp">
-        Temperature: {temp.main.temp} <br />
+      <div>
+        <FormInput value="Atlanta" callBack={this.formCallBack}/>
+        <div className="temp">
+          Temperature: {this.state.temperature} <br />
+        </div>
       </div>
+      
     );
   }
 }
