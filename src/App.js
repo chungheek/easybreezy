6f64dcca3673ffa73bb7c6268fce516a4df13971
Nick Tensing <tensingn@oregonstate.edu>
2020-07-15 13:46:29 -0400

    copying all of dev into nick

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
+import HttpsRedirect from 'react-https-redirect'
 import logo from './logo.svg';
 import './App.css';
+import Temperature from './components/Temperature/Temperature';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HttpsRedirect>
+      <div className="App">
+        <header className="App-header">
+          <Temperature />
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    </HttpsRedirect>
   );
 }
 
