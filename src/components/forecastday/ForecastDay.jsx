4be988b571ec68584184e0eee8002aeb305fd7aa
Nick Tensing <tensingn@oregonstate.edu>
2020-07-16 11:00:26 -0400

    Adding WeekForecast and ForecastDay components

---
+++ src/components/forecastday/ForecastDay.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Temperature from '../Temperature/Temperature';
+import './styles.scss';
+
+class ForecastDay extends Component {
+  render() {
+  	const day = this.props.day;
+  	const pad = this.props.pad;
+
+  	var dd = String(day.getDate() + pad).padStart(2, '0');
+    var mm = String(day.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = day.getFullYear();
+    var stringDay = mm + '/' + dd + '/' + yyyy;
+
+    return(
+      <div className="forecastday">
+      	{stringDay}
+        <Temperature />
+      </div>
+    );
+  }
+}
+
+export default ForecastDay;
