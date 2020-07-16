a1aa2631edfa97167b25995f0be6dd3ae336fe8a
tensingn <50158491+tensingn@users.noreply.github.com>
2020-07-16 12:51:34 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <Temperature />
+          <WeekForecast />
           <img src={logo} className="App-logo" alt="logo" />
         </header>
       </div>
