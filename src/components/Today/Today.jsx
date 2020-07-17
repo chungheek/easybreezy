326196ef78d06e0f559641f24ddaf83ecfa5ba4b
Kathleen O'Connor <oconnoka@oregonstate.edu>
2020-07-17 12:41:06 -0400

    Adding temperature component files

---
+++ src/components/Today/Today.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Today.scss';
+import temp from '../../API/temperatureApi';
+import Temperature from '../Temperature/Temperature';
+
+const today = () => {
+    return (
+    <div className="today-container">
+        <h1>Today's Forecast</h1>
+        <p>New York, New York</p>
+        <p>{temp.weather.main}</p>
+        <Temperature />
+        <p>Feels like: {temp.main.feels_like}°</p>
+    </div>
+    )
+};
+
+export default today;
