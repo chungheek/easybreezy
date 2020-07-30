253900c4bf8ed728531e21e3e356a219b43990a8
penninkn <morrikai@oregonstate.edu>
2020-07-29 21:28:23 -0400

    added toggle button for C/F conversion

--- src/components/Temperature/Temperature.jsx
+++ src/components/Temperature/Temperature.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import temp from '../../API/temperatureApi';
 import './styles.scss';
+import Toggle from '../Toggle/Toggle';
 
 class Temperature extends Component {
   render() {
     return(
-      <div className="temp">
-        Temperature: {temp.main.temp} <br />
+      <div className="temp"> 
+      Temperature:
+        <Toggle
+          temp={temp.main.temp} 
+          />
       </div>
+      
     );
   }
 }
