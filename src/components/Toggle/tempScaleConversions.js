2e9ec7fce67b27bd19e82d0e4794a5ad278d3f13
Nick Tensing <tensingn@oregonstate.edu>
2020-08-09 22:39:11 -0400

    Removing unnecessary comments

--- src/components/Toggle/tempScaleConversions.js
+++ src/components/Toggle/tempScaleConversions.js
@@ -10,7 +10,6 @@ export let kelvinToCelsius = (tempInK) => {
   return tempInK - 273.15;
 };
 
-//you apparently can't get directly from K to F; you have to convert to C first :)
 export let kelvinToFahrenheit = (tempInK) => {
   return celsiusToFahrenheit(kelvinToCelsius(tempInK));
 };
