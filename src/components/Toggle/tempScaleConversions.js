34ba092d1d9323d24e001885a24c3935c859cc5d
penninkn <morrikai@oregonstate.edu>
2020-08-09 22:02:11 -0400

    fixed Magic Number code smell in toggle component

--- src/components/Toggle/tempScaleConversions.js
+++ src/components/Toggle/tempScaleConversions.js
@@ -1,13 +1,18 @@
+const F_TO_C = 5/9;
+const C_TO_F = 9/5;
+const THIRTY_TWO_DEGREES = 32;
+const K_TO_C = 273.15
+
 export let fahrenheitToCelsius = (tempInF) => {
-    return (tempInF -32) * 5/9;
+    return (tempInF - THIRTY_TWO_DEGREES) * F_TO_C;
 };
 
 export let celsiusToFahrenheit = (tempInC) => {
-    return (tempInC*9/5 + 32);
+    return (tempInC * C_TO_F + THIRTY_TWO_DEGREES);
 };
 
 export let kelvinToCelsius = (tempInK) => {
-    return tempInK - 273.15 
+    return tempInK - K_TO_C; 
 };
 
 //you apparently can't get directly from K to F; you have to convert to C first :)
