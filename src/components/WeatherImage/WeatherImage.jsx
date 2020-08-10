db96c65b226c66cd8a2775b257f247ecaf56c6e6
Kathleen O'Connor <oconnoka@oregonstate.edu>
2020-08-09 20:26:57 -0400

    Updated formatting on WeatherImage.jsx

--- src/components/WeatherImage/WeatherImage.jsx
+++ src/components/WeatherImage/WeatherImage.jsx
@@ -5,10 +5,17 @@ class WeatherImage extends React.Component {
   render() {
     return (
       <div className="weatherImage">
-        <img alt="weather" src={"http://openweathermap.org/img/wn/" + this.props.weatherImage + "@2x.png"} />
+        <img
+          alt="weather"
+          src={
+            'http://openweathermap.org/img/wn/' +
+            this.props.weatherImage +
+            '@2x.png'
+          }
+        />
       </div>
     );
   }
-  }
-  
-  export default WeatherImage;
+}
+
+export default WeatherImage;
