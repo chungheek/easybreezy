9b521169d98aa7513846b2365b7f9841d9933d46
Nick Tensing <tensingn@oregonstate.edu>
2020-08-11 23:59:35 -0400

    Adding Wind image

--- src/components/Wind/Wind.jsx
+++ src/components/Wind/Wind.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Wind.scss';
+import windImg from '../images/wind.png'
 
 const Wind = (props) => {
   return (
     props.speed && (
       <div>
+          <img className="wind-img" src={windImg} alt="wind" />
           Wind: {props.speed} (m/s)
           <br />
       </div>
